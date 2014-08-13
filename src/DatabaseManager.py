c388e22c2e30fa9e4217fc02d2ad3262620a1965
Archer Reilly <archerreilly@gmail.com>
2014-08-13 10:10:45 +0800

    DatabaseManager

---
+++ src/DatabaseManager.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+#coding=utf-8
+#
+# Author: Archer Reilly
+# Date: 12/Aug/2014
+# File: DatabaseManager.py
+# Description: class for database related operations
+# Website: http://csrgxtu.blog.com
+#
+# Produced By CSRGXTU
+import MySQLdb as MySQL
+
+class DatabaseManager(object):
+  
+  host = 'localhost'
+  port = '3306'
+  userName = 'root'
+  passWord = 'root'
+  dataBase = 'football_db'
+
+  conn = None
+  curs = None
+
+  def __init__(self):
+    self.conn = MySQL.connect(host = self.host,\
+      user = self.userName, passwd = self.passWord,\
+      db = self.dataBase)
+    self.curs = self.conn.cursor()
+
+  # execSql
+  # execute an sql statement
+  #
+  # @parameter sqlStatement (string)
+  # @return void
+  def execSql(self, sqlStatement):
+    self.curs.execute(sqlStatement)
+
+  # getResults
+  # get the execute results
+  #
+  # @return list
+  def getResults(self):
+    return self.curs.fetchall()
+
+  # test
+  #
+  def test(self):
+    for row in self.curs.fetchall():
+      print row[1]
+
+  def gethost(self):
+    return self.host
+
+  def getPort(self):
+    return self.port
+
+  def getUserName(self):
+    return self.userName
+
+  def getPassWord(self):
+    return self.passWord
+
+  def getDataBase(self):
+    return self.dataBase
+
+  def getConn(self):
+    return self.conn
+
+  def getCurs(self):
+    return self.curs
+
+  def setHost(self, host):
+    self.host = host
+
+  def setPort(self, port):
+    self.port = port
+
+  def setUserName(self, userName):
+    self.userName = userName
+
+  def setPassWord(self, passWord):
+    self.passWord = passWord
+
+  def setDataBase(self, dataBase):
+    self.dataBase = dataBase
+
+  def setConn(self, conn):
+    self.conn = conn
+
+  def setCurs(self, curs):
+    self.curs = curs
+
+  def closeCurs(self):
+    self.curs.close()
+
+  def closeConn(self):
+    self.conn.close()
+
+  def __del__(self):
+    self.closeCurs()
+    self.closeConn()
