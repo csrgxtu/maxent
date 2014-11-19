299ae47a29320397d738813da61051dbe0987b27
csrgxtu <archerreilly@gmail.com>
2014-11-19 22:02:50 +0800

    geted the dates in all seasons will be used later, approxmotely 3900 matrixes

---
+++ src/basketball/SortByTime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding = utf-8
+# Author: Archer Reilly
+# Date: 19/Nov/2014
+# File: SortByTime.py
+# Desc: sort the content in original data file by time
+#
+# Produced By CSRGXTU
+from Utility import loadMatrixFromFile, saveMatrixToFile
+from os import listdir
+from datetime import datetime
+
+"""
+matrix = loadMatrixFromFile('/home/archer/Documents/maxent/data/basketball/1610612766.csv')
+print matrix
+"""
+
+dirs = listdir('/home/archer/Documents/maxent/data/basketball/')
+for f in dirs:
+  if f.startswith('161'):
+    print 'Process file: ' + f
+    matrix = loadMatrixFromFile('/home/archer/Documents/maxent/data/basketball/' + f)
+    matrixa = sorted(matrix, key=lambda x: datetime.strptime(x[1], '%b %d:%Y'))
+    saveMatrixToFile('/home/archer/Documents/maxent/data/basketball/' + f + '.sorted', matrixa)
--- src/basketball/Utility.py
+++ src/basketball/Utility.py
@@ -50,6 +50,29 @@ def readmatricefromfile(inputFile):
 	# with open(inputFile, "r") as myFile:
 	# 	pass
 
+# loadMatrixFromFile
+# load string matrix from file
+#
+# @param inputFile
+# @return lst 2 dim
+def loadMatrixFromFile(inputFile):
+  res = []
+  with open(inputFile, 'r') as myFile:
+    for line in myFile:
+      res.append(line.rstrip().split(','))
+  return res
+
+# saveMatrixToFile
+# save an string matrix to file
+#
+# @param outputFile
+# @param matrix
+# @return noe
+def saveMatrixToFile(outputFile, matrix):
+  with open(outputFile, 'w') as myFile:
+    for row in matrix:
+      myFile.write(','.join([str(x) for x in row]) + '\n')
+
 # loadTeamIds
 # load team ids from file
 #
