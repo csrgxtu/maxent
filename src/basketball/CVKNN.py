554c415a81386107e55d41098dc4eb70f9f7c4dd
csrgxtu <archerreilly@gmail.com>
2014-12-23 22:10:02 +0800

    KNN source structure builded

---
+++ src/basketball/CVKNN.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding = utf-8
+#
+# Author: Archer Reilly
+# Date: 23/DEC/2014
+# File: CVKNN.py
+# Desc: KNN -- K Nearest Neighbours, use KNN classifier
+#
+# Produced By CSRGXTU
+import cv2
+import numpy as np
+
+# buildTrainingSets
+# build training sets from raw data file
+#
+# @param inputFile
+# @return numpy.ndarray
+def buildTrainingSets(inputFile):
+  pass
+
+# buildTestingSets
+# build testing sets from raw data file
+#
+# @param inputFile
+# @return numpy.ndarray
+def buildTestingSets(inputFile):
+  pass
+
+# teamMain
+# train and test for team
+def teamMain():
+  pass
+
+# main
+# train and test for total
+def main():
+  pass
+
+if __name__ == '__main__':
+  pass
