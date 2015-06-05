dfc38e596252afba642a9c2203d08a1d1f2622ed
archer <1246506786@qq.com>
2015-06-05 01:15:07 -0700

    inserted TeamStats RegularSeasons

--- src/basketball/migration/InsertTeamStats.py
+++ src/basketball/migration/InsertTeamStats.py
@@ -123,11 +123,95 @@ def insertPlayoff(teamIds, cur):
             # print sql, seasonID
             # print row[6], row[6][0:3], row[6][-3:], teamID, opponentTeamID, home
 
-def insertRegular(teamIds, seasons):
-    pass
+def insertRegular(teamIds, seasons, cur):
+    basePath = '/home/archer/Documents/Python/maxent/data/basketball/leaguerank/'
+    for team in teamIds:
+        for season in seasons:
+            print 'INFO: ', team, season
+            matrix = loadMatrixFromFile(basePath + team + '.' + season + '.csv.sorted.csv')
+            for row in matrix:
+                row = none20(row)
+
+                teamID = findId(row[5][0:3])
+                opponentTeamID = findId(row[5][-3:])
+                home = isHome(row[5])
+
+                sql = "select SeasonID from Season where Season = '%s' and Season_SeasonTypeID = 2" % row[2]
+                cur.execute(sql)
+                seasonID = cur.fetchone()[0]
+
+                sql = "insert into TeamStats (\
+                                    TeamStats_TeamID,\
+                                    TeamStats_SeasonID,\
+                                    Result,\
+                                    Date,\
+                                    Home,\
+                                    Fgm,\
+                                    Fga,\
+                                    3pm,\
+                                    3pa,\
+                                    Ftm,\
+                                    Fta,\
+                                    Oreb,\
+                                    Dreb,\
+                                    Ast,\
+                                    Stl,\
+                                    Blk,\
+                                    Tov,\
+                                    Pf,\
+                                    CreatedBy,\
+                                    CreatedTime,\
+                                    Points,\
+                                    OpponentTeamID) value (\
+                                    %d,\
+                                    %d,\
+                                    '%c',\
+                                    '%s',\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    %d,\
+                                    'archer',\
+                                    '2015-06-05 15:52:22',\
+                                    %d,\
+                                    %d)" %\
+                                    (\
+                                        int(teamID),\
+                                        int(seasonID),\
+                                        row[0],\
+                                        row[1],\
+                                        int(home),\
+                                        int(row[6]),\
+                                        int(row[7]),\
+                                        int(row[8]),\
+                                        int(row[9]),\
+                                        int(row[10]),\
+                                        int(row[11]),\
+                                        int(row[12]),\
+                                        int(row[13]),\
+                                        int(row[14]),\
+                                        int(row[15]),\
+                                        int(row[16]),\
+                                        int(row[17]),\
+                                        int(row[18]),\
+                                        int(row[19]),\
+                                        int(opponentTeamID)
+                                    )
+                cur.execute(sql)
 
 con = mdb.connect('localhost', 'root', 'root', 'NBA')
 
 with con:
     cur = con.cursor()
-    insertPlayoff(teamIds, cur)
+    # insertPlayoff(teamIds, cur)
+    insertRegular(teamIds, seasons, cur)
